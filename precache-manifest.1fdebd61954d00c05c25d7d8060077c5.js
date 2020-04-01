4647d5977f037ae3993f447b00917e5bd59d28a5
Konrad <konrad.stochel.13@gmail.com>
2020-04-01 20:26:46 +0200

    Updates

---
+++ precache-manifest.1fdebd61954d00c05c25d7d8060077c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff1e4c1e65ecac810e04670ee68d544",
+    "url": "/ApteczkaProject/index.html"
+  },
+  {
+    "revision": "bad679d2946ecd55b813",
+    "url": "/ApteczkaProject/static/js/2.6a804407.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ApteczkaProject/static/js/2.6a804407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab41fa03325deda0e58",
+    "url": "/ApteczkaProject/static/js/main.9a69935a.chunk.js"
+  },
+  {
+    "revision": "93fe5c0a11df7624ae45",
+    "url": "/ApteczkaProject/static/js/runtime-main.81ce0c0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ApteczkaProject/precache-manifest.ae359c9c9c7ab5047dd8b3e0b88f23f5.js"
+  "/ApteczkaProject/precache-manifest.1fdebd61954d00c05c25d7d8060077c5.js"
 );
 
 self.addEventListener('message', (event) => {
