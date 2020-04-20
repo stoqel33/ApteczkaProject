de0349e4ea7012220b80cf5f36a26541fff4a2a2
Konrad <konrad.stochel.13@gmail.com>
2020-04-20 11:36:04 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ApteczkaProject/precache-manifest.29338463feacb9fd709b1df260424ab5.js"
+  "/ApteczkaProject/precache-manifest.ebeaea6a6c7b4a6ccefb9f99cc34c6ca.js"
 );
 
 self.addEventListener('message', (event) => {
