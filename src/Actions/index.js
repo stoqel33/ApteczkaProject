fd4120b0af27bbb6ab79a6d673a2e7a31d4445fb
stoqel33 <stochel202@interia.pl>
2020-04-24 22:37:34 +0200

    add actions, get, change and delete medicine

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -6,7 +6,6 @@ export const fetchMedicines = () => (dispatch) => {
   return axios
     .get('http://localhost:3000/ApteczkaProject/')
     .then(({ data }) => {
-      console.log('data w leki', data);
       dispatch({
         type: 'FETCH_SUCCESS',
         payload: {
@@ -19,42 +18,50 @@ export const fetchMedicines = () => (dispatch) => {
       dispatch({ type: 'FETCH_FAILURE' });
     });
 };
-
-// export const fetchOneMedicine = (id) => (dispatch) => {
-//   dispatch({ type: 'FETCH_REQUEST' });
-//   return axios
-//     .get(`http://localhost:3000/ApteczkaProject/editMedicine/${id}`)
-//     .then(({ data }) => {
-//       console.log('data 1 lek', data);
-//       dispatch({
-//         type: 'FETCH_SUCCESS',
-//         payload: {
-//           data,
-//         },
-//       });
-//     });
-// };
-
-export const changeMedicine = (medicine, id) => {};
-
-export const addMedicine = (medicine) => {
-  const getID = () => `_${Math.random().toString(36).substr(2, 9)}`;
-  return {
-    type: 'ADD_MED',
-    payload: {
-      item: {
-        id: getID(),
-        ...medicine,
-      },
-    },
-  };
+export const changeMedicine = (medicine, id) => (dispatch) => {
+  dispatch({ type: 'CHANGE_MED_REQUEST' });
+  return axios
+    .post(`http://localhost:3000/ApteczkaProject/editMedicine/update/${id}`, {
+      type: medicine,
+    })
+    .then(({ data }) => {
+      dispatch({
+        type: 'CHANGE_MED_SUCCESS',
+        payload: {
+          data,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch({ type: 'FETCH_FAILURE' });
+    });
 };
 
-export const removeMedicine = (id) => {
-  return {
-    type: 'REMOVE_MED',
-    payload: {
-      id,
-    },
-  };
+export const addMedicine = (medicine) => (dispatch) => {
+  dispatch({ type: 'ADD_MED_REQUEST' });
+  return axios
+    .post('http://localhost:3000/ApteczkaProject/addMedicine', {
+      type: medicine,
+    })
+    .then(({ data }) => {
+      dispatch({
+        type: 'ADD_MED_SUCCESS',
+        payload: {
+          data,
+        },
+      });
+    });
+};
+
+export const removeMedicine = (id) => (dispatch) => {
+  dispatch({ type: 'REMOVE_MED_REQUEST' });
+  axios.delete(`http://localhost:3000/ApteczkaProject/editMedicine/${id}`).then(() => {
+    dispatch({
+      type: 'REMOVE_MED_SUCCESS',
+      payload: {
+        id,
+      },
+    });
+  });
 };
