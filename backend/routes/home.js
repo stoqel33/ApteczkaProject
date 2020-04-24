5a2a6bada0766588da11910e9f8443b43da64dac
stoqel33 <stochel202@interia.pl>
2020-04-24 22:35:23 +0200

    update backend routes

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -4,45 +4,39 @@ const Medicines = require('../models/medicines.model');
 
 router.route('/').get((req, res) => {
   Medicines.find()
-    .then((medicines) => res.json(medicines))
+    .then((data) => res.json(data))
     .catch((err) => res.status(400).json(`Error ${err}`));
 });
 router.route('/addMedicine').post((req, res) => {
-  const { name } = req.body;
-  const amount = Number(req.body.amount);
-  const expiryDate = Date.parse(req.body.expiryDate);
-
+  const { name } = req.body.type;
+  const amount = Number(req.body.type.amount);
+  const expiryDate = Date.parse(req.body.type.date);
   const newMedicine = new Medicines({
     name,
     amount,
     expiryDate,
   });
-
   newMedicine
     .save()
-    .then(() => res.json('New medicine added!'))
-    .catch((err) => res.status(400).json(`Error${err}`));
-});
-router.route('/editMedicine/:id').get((req, res) => {
-  Medicines.findById(req.params.id)
-    .then((medicine) => res.json(medicine))
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .then(() => res.json(newMedicine))
+    .catch((err) => res.status(400).json(`Error${err} baddd`));
 });
 router.route('/editMedicine/:id').delete((req, res) => {
   Medicines.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Medicine deleted!'))
+    .then(() => {
+      res.json('Medicine deleted!');
+    })
     .catch((err) => res.status(400).json(`Error ${err}`));
 });
 router.route('/editMedicine/update/:id').post((req, res) => {
   Medicines.findById(req.params.id)
     .then((medicine) => {
-      medicine.name = req.body.name;
-      medicine.amount = Number(req.body.amount);
-      medicine.expiryDate = Date.parse(req.body.expiryDate);
-
+      medicine.name = req.body.type.name;
+      medicine.amount = Number(req.body.type.amount);
+      medicine.expiryDate = Date.parse(req.body.type.date);
       medicine
         .save()
-        .then(() => res.json('Medicine updated!'))
+        .then(() => res.json(medicine))
         .catch((err) => res.status(400).json(`Error${err}`));
     })
     .catch((err) => res.status(400).json(`Error ${err}`));
