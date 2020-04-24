604beb1468b7a939f65135b3937b18d47a3fafd6
stoqel33 <stochel202@interia.pl>
2020-04-24 22:37:51 +0200

    add reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,25 +1,33 @@
-const initialState = {};
+const initialState = {
+  isLoading: false,
+};
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'FETCH_REQUEST':
+      return {
+        ...state,
+        isLoading: true,
+      };
     case 'FETCH_SUCCESS':
       return {
         medicines: [...action.payload.data],
+        isLoading: false,
       };
-    case 'ADD_MED':
+    case 'ADD_MED_SUCCESS':
       return {
-        medicines: [...state.medicines, action.payload.item],
+        medicines: [...state.medicines, action.payload.data],
+        isLoading: false,
       };
-    case 'REMOVE_MED':
+    case 'REMOVE_MED_SUCCESS':
       return {
         medicines: [
           ...state.medicines.filter((medicine) => medicine.id !== action.payload.id),
         ],
+        isLoading: false,
       };
     default:
       return state;
   }
 };
-// console.log(initialState);
-
 export default rootReducer;
