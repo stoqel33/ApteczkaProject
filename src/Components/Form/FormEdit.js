baf951071a84f476df2d96b5847ec247df4853f6
stoqel33 <stochel202@interia.pl>
2020-04-25 20:33:38 +0200

    feature edit medicine form

--- src/Components/Form/FormEdit.js
+++ src/Components/Form/FormEdit.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import axios from 'axios';
-import Link from 'Styled/Link';
-import Label from './Label/Label';
+import { Formik, Form } from 'formik';
+import { useHistory, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { changeMedicine, removeMedicine } from 'Actions';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,22 +11,48 @@ const Wrapper = styled.div`
   align-items: center;
   min-height: 100vh;
 `;
-
 const Title = styled.h1`
   margin: 20px 0;
   color: white;
 `;
-
-const FormAdd = styled.form`
+const MedicineLabel = styled.label`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media screen and (min-width: 800px) {
+    .medicineLabel {
+      margin: 0 80px;
+    }
+  }
+`;
+const Input = styled.input`
+  color: white;
+  padding: 10px 0 10px 15px;
+  margin-right: 10px;
+  background-color: transparent;
+  border: none;
+  border-left: 1px solid black;
+  border-bottom: 1px solid black;
+  ::placeholder {
+    font-size: 300;
+    color: white;
+  }
+`;
+const InputAmount = styled(Input)`
+  width: 50px;
+`;
+const TitleMed = styled.h2`
+  font-size: 20px;
+  margin-left: 10px;
+`;
+const Forms = styled(Form)`
   width: 100%;
 `;
-
 const WrapperMedicine = styled.div`
   padding: 20px 5px;
   margin: 30px 0;
   background: rgba(245, 245, 245, 0.3);
 `;
-
 const ButtonAdd = styled.button`
   width: 250px;
   height: 50px;
@@ -43,119 +70,111 @@ const ButtonAdd = styled.button`
   cursor: pointer;
 `;
 
-class FormEdit extends React.Component {
-  state = {
-    id: '',
-    nameMedicine: '',
-    amountMedicine: '',
-    dateMedicine: '',
+const FormEdit = ({ medicine, changeMed, removeMed }) => {
+  const today = new Date().toISOString().slice(0, 10);
+  const history = useHistory();
+  const backToHome = () => {
+    history.push('/ApteczkaProject');
   };
-
-  // Walidacja inputów
-  handleInputChange = (e) => {
-    if (e.target.type === 'tel') {
-      e.target.value = e.target.value.replace(/[^0-9]+/, '');
-      if (e.target.value === '0') {
-        e.target.value = '0';
-      } else {
-        this.setState({
-          [e.target.id]: e.target.value,
-        });
-      }
-    } else if (e.target.type === 'text') {
-      e.target.value = e.target.value.replace(/[^a-zA-Z]+/, '');
-      this.setState({
-        [e.target.id]: e.target.value,
-      });
-    } else {
-      const nowDate = new Date(this.props.today);
-      const currentDate = new Date(e.target.value);
-      if (currentDate >= nowDate) {
-        this.setState({
-          [e.target.id]: e.target.value,
-        });
-      }
-    }
-  };
-
-  componentDidMount() {
-    axios
-      .get('http://localhost:3000/ApteczkaProject/editMedicine/' + this.props.url)
-
-      .then((resp) => {
-        this.setState({
-          id: resp.data._id,
-          nameMedicine: resp.data.name,
-          amountMedicine: resp.data.amount.toString(),
-          dateMedicine: resp.data.expiryDate.slice(0, 10),
-        });
-      });
-  }
-
-  // Obsługa edycji leku
-  handleClickSubmit = () => {
-    const { id, nameMedicine, amountMedicine, dateMedicine } = this.state;
-    const nameMed = nameMedicine.charAt(0).toUpperCase() + nameMedicine.slice(1);
-    this.props.changeMedicine(id, nameMed, amountMedicine, dateMedicine);
-  };
-
-  render() {
-    return (
-      <Wrapper>
-        <Title>Edytuj Lek</Title>
-        <FormAdd>
-          <>
+  return (
+    <Wrapper>
+      <Title>Dodaj Lek</Title>
+      <Formik
+        initialValues={{
+          name: medicine[0].name,
+          amount: medicine[0].amount,
+          date: medicine[0].expiryDate.slice(0, 10),
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.name) {
+            errors.name = 'Wpisz nazwę leku!';
+          } else if (/[^a-zA-Z]+/i.test(values.name)) {
+            errors.name = 'Nazwa zawiera niedozwolone znaki';
+          }
+          if (!values.amount) {
+            errors.amount = 'Podaj ilość leku!';
+          } else if (values.amount < 0) {
+            errors.amount = 'Podaj poprawną ilość';
+          }
+          if (!values.date) {
+            errors.date = 'Podaj datę ważności!';
+          } else if (values.date < today) {
+            errors.date = 'Nie możesz wprowadzić starego leku';
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+          changeMed(values, medicine[0]._id);
+          backToHome();
+        }}
+      >
+        {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
+          <Forms onSubmit={handleSubmit}>
             <WrapperMedicine>
-              <Label
-                name="nameMedicine"
-                type="text"
-                placeholder="Nazwa"
-                autoComplete="off"
-                value={this.state.nameMedicine}
-                onChange={this.handleInputChange}
-              >
-                Nazwa leku
-              </Label>
-            </WrapperMedicine>
-            <WrapperMedicine>
-              <Label
-                name="amountMedicine"
-                type="tel"
-                placeholder="0"
-                autoComplete="off"
-                minAmount="1"
-                value={this.state.amountMedicine}
-                onChange={this.handleInputChange}
-              >
-                Ilość Tabletek
-              </Label>
+              <MedicineLabel>
+                <TitleMed>Nazwa</TitleMed>
+                <Input
+                  name="name"
+                  type="text"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={values.name}
+                />
+                {errors.name && touched.name && errors.name}
+              </MedicineLabel>
             </WrapperMedicine>
 
             <WrapperMedicine>
-              <Label
-                name="dateMedicine"
-                type="date"
-                minDate={this.props.today}
-                value={this.state.dateMedicine}
-                onChange={this.handleInputChange}
-              >
-                Data ważności
-              </Label>
+              <MedicineLabel>
+                <TitleMed>Ilość</TitleMed>
+                <InputAmount
+                  name="amount"
+                  type="number"
+                  placeholder="0"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={values.amount}
+                />
+                {errors.amount && touched.amount && errors.amount}
+              </MedicineLabel>
             </WrapperMedicine>
-          </>
-        </FormAdd>
-        {this.state.nameMedicine.length > 2 && this.state.amountMedicine.length > 0 ? (
-          <Link to="/ApteczkaProject" onClick={this.handleClickSubmit}>
-            <ButtonAdd>Zapisz zmiany</ButtonAdd>
-          </Link>
-        ) : (
-          <Link to="/ApteczkaProject/editMedicine">
-            <ButtonAdd>Zapisz zmiany</ButtonAdd>
-          </Link>
+
+            <WrapperMedicine>
+              <MedicineLabel>
+                <TitleMed>Data ważności</TitleMed>
+                <Input
+                  name="date"
+                  type="date"
+                  onChange={handleChange}
+                  value={values.date}
+                />
+                {errors.date && touched.date && errors.date}
+              </MedicineLabel>
+            </WrapperMedicine>
+            <ButtonAdd
+              as={Link}
+              to="/ApteczkaProject"
+              onClick={() => {
+                removeMed(medicine[0]._id);
+                backToHome();
+              }}
+            >
+              Usuń lek
+            </ButtonAdd>
+            <ButtonAdd type="submit" disabled={isSubmitting}>
+              Zapisz i wyjdz
+            </ButtonAdd>
+          </Forms>
         )}
-      </Wrapper>
-    );
-  }
-}
+      </Formik>
+    </Wrapper>
+  );
+};
 
-export default FormEdit;
+const mapDispatchToProps = (dispatch) => ({
+  changeMed: (medicine, id) => dispatch(changeMedicine(medicine, id)),
+  removeMed: (id) => dispatch(removeMedicine(id)),
+});
+
+export default connect(null, mapDispatchToProps)(FormEdit);
