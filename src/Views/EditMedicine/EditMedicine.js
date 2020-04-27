f9d92385ac8094c0e38e5ce00f970fc76e421f30
stoqel33 <stochel202@interia.pl>
2020-04-27 22:33:33 +0200

    change EditMedicine to functional component

--- src/Views/EditMedicine/EditMedicine.js
+++ src/Views/EditMedicine/EditMedicine.js
@@ -1,55 +1,19 @@
 import React from 'react';
-// import AppContext from 'context';
-import axios from 'axios';
+import { connect } from 'react-redux';
 import FormEdit from 'Components/Form/FormEdit';
 
-class EditMedicine extends React.Component {
-  state = {
-    id: '',
-    today: new Date().toISOString().slice(0, 10),
-    medicines: [],
-  };
+const EditMedicine = ({ medicine }) => {
+  return (
+    <>
+      <FormEdit medicine={medicine} />
+    </>
+  );
+};
 
-  changeMedicine = (id, name, amount, date) => {
-    const allMedicines = this.state.medicines;
-    allMedicines[id] = {
-      id,
-      name,
-      amount,
-      date,
-    };
-
-    this.setState({
-      medicines: allMedicines,
-      id,
-    });
-
-    const newMed = {
-      name,
-      amount,
-      expiryDate: date,
-    };
-
-    axios
-      .post('http://localhost:3000/ApteczkaProject/editMedicine/update/' + id, newMed)
-      .then((res) => console.log(res.data));
-    window.location = '/ApteczkaProject';
-  };
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <>
-        <FormEdit
-          medicines={this.state.medicines}
-          changeMedicine={this.changeMedicine}
-          today={this.state.today}
-          url={this.props.match.params.id}
-        />
-      </>
-    );
-  }
-}
-
-export default EditMedicine;
+const mapStateToProps = (state, ownProps) => {
+  const { medicines } = state;
+  const url = ownProps.match.params.id;
+  const medicine = medicines.filter((item) => item._id === url);
+  return { medicine };
+};
+export default connect(mapStateToProps)(EditMedicine);
