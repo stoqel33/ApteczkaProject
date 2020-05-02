4ddd7d2cb5a06377ac9445bebd3cd3d4efdc463c
stoqel33 <stochel202@interia.pl>
2020-05-02 14:07:14 +0200

    update AddMedicine view and change to function component

--- src/Views/AddMedicine/AddMedicine.js
+++ src/Views/AddMedicine/AddMedicine.js
@@ -1,33 +1,12 @@
 import React from 'react';
-// import AppContext from 'context';
-import axios from 'axios';
 import FormAdd from 'Components/Form/FormAdd';
 
-class AddMedicine extends React.Component {
-  addMedicine = (name, amount, date) => {
-    const newMedicine = {
-      name: name,
-      amount: Number(amount),
-      expiryDate: date,
-    };
-
-    // this.setState((prevState) => ({
-    //   medicines: [...prevState.medicines, newMedicine],
-    // }));
-
-    axios
-      .post('http://localhost:3000/ApteczkaProject/addMedicine', newMedicine)
-      .then((res) => console.log(res.data));
-  };
-
-  render() {
-    return (
-      <>
-        <FormAdd match={this.props} />
-        {console.log(this.props)};
-      </>
-    );
-  }
-}
+const AddMedicine = () => {
+  return (
+    <>
+      <FormAdd />
+    </>
+  );
+};
 
 export default AddMedicine;
