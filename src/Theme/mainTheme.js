4ebd4d3128eac0295ca78241564f66efed8ffcb3
stoqel33 <stochel202@interia.pl>
2020-05-29 19:26:49 +0200

    add colors warning and secondary

--- src/Theme/mainTheme.js
+++ src/Theme/mainTheme.js
@@ -7,6 +7,9 @@ export const theme = {
     colors: {
       background: 'hsl(210, 7%, 94%)',
       primary: 'hsl(233, 12%, 26%)',
+      secondary: 'hsl(51, 82%, 56%)',
     },
   },
+
+  warning: 'hsl(0, 51%, 46%)',
 };
