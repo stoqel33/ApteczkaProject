659726941b95f6b8015fef31240512ca481ade46
stoqel33 <stochel202@interia.pl>
2020-06-04 19:12:31 +0200

    update molecule Card

--- src/Components/molecules/Card/Card.js
+++ src/Components/molecules/Card/Card.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css, keyframes } from 'styled-components';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeOneMedicine } from 'data/Actions';
+import { Link } from 'react-router-dom';
 
 import Title from 'Components/atoms/Title/Title';
 import ButtonIcon from 'Components/atoms/ButtonIcon/ButtonIcon';
@@ -9,25 +12,55 @@ import ExpiredBar from 'Components/atoms/ExpiredBar/ExpiredBar';
 import pillIcon from 'assets/icons/pill.svg';
 import infoIcon from 'assets/icons/info.svg';
 import editIcon from 'assets/icons/edit.svg';
+import exitIcon from 'assets/icons/exit.svg';
 
+const showing = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 7rem 7rem;
-  grid-template-rows: 3.5rem 3.5rem;
+  grid-template-rows: 4.5rem 3.5rem;
   grid-template-areas:
     'medicine info info'
     'medicine button button';
 
   width: 30rem;
-  height: 8rem;
+  height: 9rem;
   padding: 0.5rem;
-  border-radius: ${({ expired }) =>
-    expired ? '2rem 2rem 0 0' : ' 2rem 2rem 0.9rem 0.9rem'};
-  border: ${({ expired }) => (expired ? '2px solid red' : '2px solid black')};
+  margin-bottom: 2rem;
+  border-radius: 2rem 2rem 0.9rem 0.9rem;
+  border: 2px solid black;
   border-bottom: none;
+  box-shadow: 0px 6px 10px -5px ${({ theme }) => theme.lightmode.colors.secondary};
   background-color: ${({ theme }) => theme.lightmode.colors.background};
-`;
+  transition: 0.4s;
 
+  ${({ expired }) =>
+    expired &&
+    css`
+      box-shadow: none;
+      margin-bottom: 0;
+      border-radius: 2rem 2rem 0 0;
+      border: 2px solid red;
+    `}
+  ${({ moreInfo }) =>
+    moreInfo &&
+    css`
+      height: 14rem;
+      grid-template-rows: 4.5rem 3.5rem 5rem;
+      grid-template-areas:
+        'medicine info info'
+        'medicine button button'
+        'date date date';
+    `};
+`;
 const InnerWrapper = styled.div`
   ${({ medicine }) =>
     medicine &&
@@ -46,33 +79,76 @@ const InnerWrapper = styled.div`
       justify-content: space-around;
       align-items: center;
     `}
-    ${({ info }) =>
-      info &&
-      css`
-        grid-area: info;
-        display: flex;
-        justify-content: flex-end;
-        align-items: flex-start;
-        margin-right: 0.5rem;
-        margin-top: 0.3rem;
-      `}
+  ${({ info }) =>
+    info &&
+    css`
+      grid-area: info;
+      display: flex;
+      justify-content: flex-end;
+      align-items: flex-start;
+      margin-right: 0.5rem;
+      margin-top: 0.3rem;
+    `}
+  ${({ date, expired }) =>
+    date &&
+    css`
+      grid-area: date;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      animation: ${showing} 0.4s;
+      color: ${() => (expired ? 'red' : 'black')};
+    `}
 `;
 
-const Card = ({ name, amount, expired }) => {
+const Card = ({ id, name, amount, date, today, takePill }) => {
+  const [info, setInfo] = useState(false);
+
+  const handleInfoEnable = () => {
+    setInfo(true);
+  };
+  const handleInfoDisable = () => {
+    setInfo(false);
+  };
+
+  const handleTakeMedicine = () => {
+    if (amount > 0) takePill(id);
+  };
+  const expired = today > date;
   return (
     <>
-      <Wrapper expired={expired}>
+      <Wrapper expired={expired} moreInfo={info}>
         <InnerWrapper medicine>
-          <Title mgb="0.5rem">{name}</Title>
-          <Title mgt="0.5rem">{amount} tabletek</Title>
+          <Title mgb="0.5rem" fw="400">
+            {name}
+          </Title>
+          <Title mgt="0.5rem" fw="400">
+            {amount} tabletek
+          </Title>
         </InnerWrapper>
         <InnerWrapper info>
-          <ButtonIcon icon={infoIcon} />
+          {info ? (
+            <ButtonIcon icon={exitIcon} onClick={handleInfoDisable} />
+          ) : (
+            <ButtonIcon icon={infoIcon} onClick={handleInfoEnable} />
+          )}
         </InnerWrapper>
         <InnerWrapper button>
-          <ButtonIcon icon={editIcon} size="2.3rem" />
-          <ButtonIcon icon={pillIcon} size="2.3rem" />
+          <ButtonIcon
+            icon={editIcon}
+            size="2.3rem"
+            as={Link}
+            to={`/ApteczkaProject/editMedicine/${id}`}
+          />
+          <ButtonIcon icon={pillIcon} size="2.3rem" onClick={handleTakeMedicine} />
         </InnerWrapper>
+        {info && (
+          <InnerWrapper date expired={expired}>
+            <Title fw="400" fs="1.8">
+              Data ważności {date}
+            </Title>
+          </InnerWrapper>
+        )}
       </Wrapper>
       {expired && <ExpiredBar>Minęła data ważności leku</ExpiredBar>}
     </>
@@ -80,13 +156,16 @@ const Card = ({ name, amount, expired }) => {
 };
 
 Card.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-  expired: PropTypes.bool,
+  date: PropTypes.string.isRequired,
+  today: PropTypes.string.isRequired,
+  takePill: PropTypes.func.isRequired,
 };
 
-Card.defaultProps = {
-  expired: false,
-};
+const mapDispatchToProps = (dispatch) => ({
+  takePill: (id) => dispatch(removeOneMedicine(id)),
+});
 
-export default Card;
+export default connect(null, mapDispatchToProps)(Card);
