bea01248f00d92a181f66891680e765f78323dce
stoqel33 <stochel202@interia.pl>
2020-06-04 19:14:21 +0200

    add MedicineList

--- src/Views/Medicine/Medicine.js
+++ src/Views/Medicine/Medicine.js
@@ -3,72 +3,17 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchMedicines } from 'Actions';
-import Link from 'Styled/Link';
-import MedicineItem from 'Components/MedicineItem/MedicineItem';
-import Search from 'Components/Search/Search';
-import MagnifyingGlass from 'Components/Icons/MagnifyingGlass.js';
-import BurgerMenu from 'Components/Icons/BurgerMenu.js';
+import { fetchMedicines } from 'data/Actions';
 import Load from '../../Components/Load/Load';
+import MedicinesList from 'templates/MedicinesList/MedicinesList';
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  text-align: center;
-`;
-const Title = styled.h1`
-  margin: 30px 0;
-  color: white;
-`;
-const TitleWrap = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-const Empty = styled.h1`
-  margin: 50px 10px;
-  padding: 100px 40px;
-  background-color: rgba(245, 245, 245, 0.3);
-  border-radius: 10%;
-  color: black;
-`;
-const Icon = styled.div`
-  top: 15px;
-  cursor: pointer;
-`;
-const ButtonAddNew = styled.button`
-  width: 100%;
-  margin-bottom: 20px;
-  font-size: 35px;
-  color: white;
-  background-color: transparent;
-  border: none;
-`;
 const Info = styled.span`
   display: flex;
   position: fixed;
   align-self: center;
   padding-top: 0px;
 `;
-const MedicinesWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 80%;
-  margin: 0 auto;
-  @media screen and (min-width: 768px) {
-    width: 100%;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-  @media screen and (min-width: 1000px) {
-    width: 100%;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-`;
+
 class Medicine extends React.Component {
   state = {
     search: false,
@@ -96,53 +41,24 @@ class Medicine extends React.Component {
   };
 
   render() {
-    const { medicines, isLoading, response } = this.props;
+    const { medicines, isLoading, response, refresh } = this.props;
     medicines.sort((a, b) => a.name.toString().localeCompare(b.name, 'pl'));
     return (
       <>
         {response ? (
-          <Wrapper>
-            <TitleWrap>
-              <Icon onClick={this.handleSearchToogle}>
-                {medicines.length > 0 && <MagnifyingGlass />}
-              </Icon>
-              <Title>Apteczka</Title>
-              <Icon>
-                <BurgerMenu />
-              </Icon>
-            </TitleWrap>
-            {isLoading && <Load />}
-            {this.state.search ? (
-              <Search
-                medicines={medicines}
-                today={this.state.today}
-                toggle={this.handleSearchToogle}
-              />
-            ) : (
-              <MedicinesWrap>
-                {medicines.length > 0 ? (
-                  medicines.map(({ name, amount, expiryDate, _id }) => (
-                    <MedicineItem
-                      key={_id}
-                      id={_id}
-                      name={name}
-                      amount={amount}
-                      date={expiryDate}
-                      today={this.state.today}
-                    />
-                  ))
-                ) : (
-                  <Empty>Brak leków</Empty>
-                )}
-              </MedicinesWrap>
-            )}
-            <ButtonAddNew>
-              <Link to="/ApteczkaProject/addMedicine">Dodaj nowy lek</Link>
-            </ButtonAddNew>
-          </Wrapper>
+          <MedicinesList
+            searchToggle={this.handleSearchToogle}
+            searching={this.state.search}
+            today={this.state.today}
+            medicines={medicines}
+            isLoading={isLoading}
+            response={response}
+            refresh={refresh}
+          />
         ) : (
           <Info>Sorry, database is down</Info>
         )}
+        {isLoading && <Load />}
       </>
     );
   }
---
+++ src/templates/MedicinesList/MedicinesList.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import Card from 'Components/molecules/Card/Card';
+import Button from 'Components/atoms/Button/Button';
+import Title from 'Components/atoms/Title/Title';
+import ButtonIcon from 'Components/atoms/ButtonIcon/ButtonIcon';
+
+import search from 'assets/icons/search.svg';
+import burger from 'assets/icons/burger-menu.svg';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  text-align: center;
+`;
+const TopBarWrap = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 2rem 0;
+`;
+const Icon = styled.div`
+  top: 15px;
+  cursor: pointer;
+`;
+const MedicinesWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    width: 100%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  @media screen and (min-width: 1000px) {
+    width: 100%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+`;
+const ButtonLink = styled(Button)`
+  height: 100%;
+  margin: 5rem auto 0;
+  padding: 1rem;
+  line-height: 100%;
+`;
+
+const MedicinesList = ({ searchToggle, searching, today, medicines }) => {
+  return (
+    <Wrapper>
+      <TopBarWrap>
+        <Icon onClick={searchToggle}>
+          {medicines.length > 0 && <ButtonIcon icon={search} size="2.8rem" />}
+        </Icon>
+        <Title fs="3">Apteczka</Title>
+        <Icon>
+          <ButtonIcon icon={burger} size="2.8rem" />
+        </Icon>
+      </TopBarWrap>
+      {searching ? null : ( // <Search medicines={medicines} today={today} toggle={searchToggle} />
+        <MedicinesWrap>
+          {medicines.length > 0 ? (
+            medicines.map(({ name, amount, expiryDate, _id }) => (
+              <Card
+                key={_id}
+                id={_id}
+                name={name}
+                amount={amount}
+                date={expiryDate.slice(0, 10)}
+                today={today}
+              />
+            ))
+          ) : (
+            <Title>Brak leków</Title>
+          )}
+        </MedicinesWrap>
+      )}
+      <ButtonLink big="true" as={Link} to="/ApteczkaProject/addMedicine">
+        Dodaj nowy lek
+      </ButtonLink>
+    </Wrapper>
+  );
+};
+
+MedicinesList.propTypes = {
+  searchToggle: PropTypes.func.isRequired,
+  searching: PropTypes.bool.isRequired,
+  today: PropTypes.string.isRequired,
+  medicines: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      expiryDate: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default MedicinesList;
