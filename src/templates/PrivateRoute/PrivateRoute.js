83fabd3b0de08acfc7ca4f9ae3e61b2e815e4309
stoqel33 <stochel202@interia.pl>
2020-06-30 10:40:55 +0200

    create feature which bloks access to private pages if user is not logged in

---
+++ src/templates/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        auth.isAuthenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/ApteczkaProject/user/signin" />
+        )
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
