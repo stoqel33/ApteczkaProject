f6c80dee2393f69a7afc7df505a6b2250f403e5c
stoqel33 <stochel202@interia.pl>
2020-06-30 13:51:43 +0200

    create private keys

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-  secretKey: 'secret',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_dev');
+} else {
+  module.exports = require('./keys_dev');
+}
