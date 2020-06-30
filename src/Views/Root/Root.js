25a15971f0b81a8998109ceefcaa8f461ae8903a
stoqel33 <stochel202@interia.pl>
2020-06-30 10:42:49 +0200

    add feature which check if token is expired, and use PrivateRoute for pages

--- src/Views/Root/Root.js
+++ src/Views/Root/Root.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from 'data/Store';
 import { ThemeProvider } from 'styled-components';
+import store from 'data/Store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from 'data/Utils/setAuthToken';
+import { setCurrentUser, logoutUser } from 'data/Actions/authActions';
+import { clearProfile } from 'data/Actions/profileActions';
 
 import { theme } from 'Theme/mainTheme';
 import GlobalStyle from 'Theme/GlobalStyle';
 import Medicine from 'Views/Medicine/Medicine';
 import AddMedicine from 'Views/AddMedicine/AddMedicine';
 import EditMedicine from 'Views/EditMedicine/EditMedicine';
+import SignIn from 'Views/SignIn/SignIn';
+import CreateProfile from 'Views/CreateProfile/CreateProfile';
 
-const Root = () => (
-  <Provider store={store}>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <Switch>
-            <Route exact path="/ApteczkaProject" component={Medicine} />
-            <Route path="/ApteczkaProject/addMedicine" component={AddMedicine} />
-            <Route path="/ApteczkaProject/editMedicine/:id" component={EditMedicine} />
-          </Switch>
-        </>
-      </ThemeProvider>
-    </Router>
-  </Provider>
-);
+import PrivateRoute from 'templates/PrivateRoute/PrivateRoute';
+
+const Root = () => {
+  // Check for token
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decoded = jwt_decode(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(decoded));
+
+    // Check if token is expired
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+      store.dispatch(logoutUser());
+      store.dispatch(clearProfile());
+      window.location.href = '/user/signin';
+    }
+  }
+  return (
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <>
+            <GlobalStyle />
+            <Switch>
+              <PrivateRoute exact path="/ApteczkaProject" component={Medicine} />
+              <PrivateRoute
+                exact
+                path="/ApteczkaProject/addMedicine"
+                component={AddMedicine}
+              />
+              <PrivateRoute
+                exact
+                path="/ApteczkaProject/editMedicine/:id"
+                component={EditMedicine}
+              />
+              <Route path="/ApteczkaProject/profile/create" component={CreateProfile} />
+            </Switch>
+            <Route path="/ApteczkaProject/user/signin" component={SignIn} />
+          </>
+        </ThemeProvider>
+      </Router>
+    </Provider>
+  );
+};
 
 export default Root;
