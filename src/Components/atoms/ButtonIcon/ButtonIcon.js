a32c60535870960b3f583effdf2dca0cad78886e
stoqel33 <stochel202@interia.pl>
2020-07-16 12:50:27 +0200

    set background color to transparent because some icons has grey backgorund

--- src/Components/atoms/ButtonIcon/ButtonIcon.js
+++ src/Components/atoms/ButtonIcon/ButtonIcon.js
@@ -4,6 +4,7 @@ const ButtonIcon = styled.button`
   width: ${({ size }) => size || '1.6rem'};
   height: ${({ size }) => size || '1.6rem'};
   background-image: url(${({ icon }) => icon});
+  background-color: transparent;
   border: none;
   cursor: pointer;
 `;
