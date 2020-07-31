90b41d8617447a1fb491cb34dd2ac8c389f1a1eb
stoqel33 <stochel202@interia.pl>
2020-07-31 14:55:44 +0200

    create errorPage color and add size for media queries

--- src/Theme/mainTheme.js
+++ src/Theme/mainTheme.js
@@ -12,5 +12,22 @@ export const theme = {
   },
 
   info: 'hsl(193.2, 77.8%, 45.9%)',
+  errorPage: 'hsl(202, 48%, 79%)',
   warning: 'hsl(0, 51%, 46%)',
+};
+
+const size = {
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  desktop: '1440px',
+};
+
+export const device = {
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  desktop: `(min-width: ${size.desktop})`,
 };
