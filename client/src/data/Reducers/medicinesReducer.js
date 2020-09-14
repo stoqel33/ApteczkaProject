ad52f3dec8b6ff3733584d05558eff38f363b96b
stoqel33 <stochel202@interia.pl>
2020-09-14 17:48:58 +0200

    refactoring reducer medicines

--- client/src/data/Reducers/medicinesReducer.js
+++ client/src/data/Reducers/medicinesReducer.js
@@ -7,69 +7,112 @@ import {
   MEDICINE_DECREASE,
 } from 'data/Actions/types';
 
+function updateObject(oldObject, newValues) {
+  return Object.assign({}, oldObject, newValues);
+}
+
+function updateItemInArray(array, itemId, updateItemCallback) {
+  const updatedItem = array.map((item) => {
+    if (item._id !== itemId) {
+      return item;
+    }
+    const updatedItem = updateItemCallback(item);
+    return updatedItem;
+  });
+  return updatedItem;
+}
+
+function removeMedicine(state, action) {
+  const newMedicines = state.medicines.filter(
+    (medicine) => medicine._id !== action.payload,
+  );
+  return {
+    medicines: newMedicines,
+  };
+}
+
+function decreaseMedicine(state, action) {
+  const newMedicine = updateItemInArray(state.medicines, action.payload, (medicine) => {
+    return updateObject(medicine, {
+      amount: medicine.amount - 1,
+    });
+  });
+  return updateObject(state, { medicines: newMedicine, loading: false });
+}
+
+function changeMedicine(state, action) {
+  const newMedicine = updateItemInArray(
+    state.medicines,
+    action.payload.medicine._id,
+    (medicine) => {
+      return updateObject(medicine, {
+        name: action.payload.medicine.name,
+        amount: action.payload.medicine.amount,
+        expiryDate: action.payload.medicine.expiryDate,
+        copy: action.payload.medicine.copy,
+      });
+    },
+  );
+  return updateObject(state, { medicines: newMedicine });
+}
+
+function addMedicine(state, action) {
+  const newMedicine = state.medicines.concat({
+    _id: action.payload._id,
+    name: action.payload.name,
+    amount: action.payload.amount,
+    expiryDate: action.payload.expiryDate,
+    copy: action.payload.copy,
+  });
+
+  return updateObject(state, { medicines: newMedicine });
+}
+
+function getMedicines(state, action) {
+  return updateObject(state, {
+    medicines: action.payload,
+    loading: false,
+    refresh: false,
+  });
+}
+
+function loading(state) {
+  return {
+    ...state,
+    loading: false,
+    refresh: false,
+  };
+}
+
 const initialState = {
   medicines: [],
   loading: false,
   refresh: false,
 };
 
-export default function (state = initialState, action) {
+function appReducer(state = initialState, action) {
   switch (action.type) {
     case MEDICINES_LOADING:
-      return {
-        ...state,
-        loading: true,
-        refresh: false,
-      };
+      return loading(state);
 
     case MEDICINES_GET:
-      return {
-        medicines: action.payload,
-        loading: false,
-        refresh: false,
-      };
+      return getMedicines(state, action);
 
     case MEDICINES_ADD:
-      return {
-        medicines: [...state.medicines, action.payload],
-        loading: false,
-      };
+      return addMedicine(state, action);
 
     case MEDICINES_CHANGE:
-      const { medicines } = state;
-      medicines.forEach((medicine) => {
-        if (medicine._id === action.payload.id) {
-          medicine.name = action.payload.data.name;
-          medicine.amount = action.payload.data.amount;
-          medicine.expiryDate = action.payload.data.expiryDate;
-        }
-      });
-      return {
-        medicines,
-        loading: false,
-      };
-
-    case MEDICINES_REMOVE:
-      return {
-        medicines: [
-          ...state.medicines.filter((medicine) => medicine._id !== action.payload.id),
-        ],
-        loading: false,
-      };
+      return changeMedicine(state, action);
 
     case MEDICINE_DECREASE:
-      const decreaseMedicine = state.medicines;
-      decreaseMedicine
-        .filter((medicine) => medicine._id === action.payload)
-        .map((medicine) => medicine.amount - 1);
-      return {
-        ...state,
-        medicines: decreaseMedicine,
-        loading: false,
-        refresh: true,
-      };
+      return decreaseMedicine(state, action);
+
+    case MEDICINES_REMOVE:
+      return removeMedicine(state, action);
 
     default:
       return state;
   }
 }
+
+export default appReducer;
