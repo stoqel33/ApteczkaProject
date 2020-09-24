e2607783946ae18100311a68d9820775a3e31e31
stoqel33 <stochel202@interia.pl>
2020-09-24 10:56:05 +0200

    update test, add mock context

--- client/src/templates/__tests__/BurgerMenu.test.js
+++ client/src/templates/__tests__/BurgerMenu.test.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { fireEvent, waitForDomChange, waitForElement } from '@testing-library/react';
+import { renderWithRouter } from 'testUtils';
 import { Provider } from 'react-redux';
 import store from 'data/Store';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'Theme/mainTheme';
-import { renderWithRouter } from 'testUtils';
 import AppContext from 'context';
 
 import BurgerMenu from 'templates/BurgerMenu/BurgerMenu';
 
-const renderBurger = (props) => {
-  const mockedContext = {
-    licencesState: false,
-    handleLicences: jest.fn(),
-  };
+const renderBurger = (mocked, ...props) => {
   const utils = renderWithRouter(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <AppContext.Provider value={mockedContext}>
+        <AppContext.Provider value={{ ...mocked }}>
           <BurgerMenu {...props} />
         </AppContext.Provider>
       </ThemeProvider>
     </Provider>,
   );
-  return { ...utils, mockedContext };
+  return { ...utils };
 };
 
 describe('Burger Menu template', () => {
@@ -32,10 +28,23 @@ describe('Burger Menu template', () => {
     expect(getByText('Wyloguj'));
   });
 
-  it('Component control button calls proper function', () => {
-    const { getByText, mockedContext } = renderBurger();
+  it('Component control button calls function', async () => {
+    const mockedContext = {
+      licencesState: false,
+      handleLicences: jest.fn(),
+    };
+    const { getByText } = renderBurger(mockedContext);
     const button = getByText('Źródła');
     fireEvent.click(button);
     expect(mockedContext.handleLicences).toBeCalledTimes(1);
+  });
+
+  it('Show proper content if licencesState is true', () => {
+    const mockedContext = {
+      licencesState: true,
+      handleLicences: jest.fn(),
+    };
+    const { getByText } = renderBurger(mockedContext);
+    expect(getByText('Ikony'));
   });
 });
