b4f35e5e9df31817f94801c137148f532c9d4b6e
stoqel33 <stochel202@interia.pl>
2021-01-20 16:59:05 +0100

    use transform scale on hover except button icon with properties 'clear'

--- client/src/Components/atoms/ButtonIcon/ButtonIcon.js
+++ client/src/Components/atoms/ButtonIcon/ButtonIcon.js
@@ -1,12 +1,28 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
+import { device } from "Theme/mainTheme";
 
 const ButtonIcon = styled.button`
-  width: ${({ size }) => size || '1.6rem'};
-  height: ${({ size }) => size || '1.6rem'};
+  width: ${({ size }) => size || "1.6rem"};
+  height: ${({ size }) => size || "1.6rem"};
   background-image: url(${({ icon }) => icon});
   background-color: transparent;
   border: none;
+  transition: 0.15s ease-in;
   cursor: pointer;
+
+  @media screen and ${device.laptop} {
+    &:hover {
+      transform: scale(1.3);
+    }
+  }
+
+  ${({ clear }) =>
+    clear &&
+    css`
+      &:hover {
+        transform: translateY(-50%);
+      }
+    `};
 `;
 
 export default ButtonIcon;
