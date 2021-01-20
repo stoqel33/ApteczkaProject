93203f15fba847ac8b654fe074b7fa97386230dd
stoqel33 <stochel202@interia.pl>
2021-01-20 16:53:14 +0100

    change button apperance, add new apperance - warning

--- client/src/Components/atoms/Button/Button.js
+++ client/src/Components/atoms/Button/Button.js
@@ -1,28 +1,40 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
+import { device } from "Theme/mainTheme";
 
 const Button = styled.button`
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   height: 4rem;
-  width: 12rem;
-  font-size: ${({ fs }) => fs || '1.4'}rem;
-  font-weight: 400;
-  color: ${({ theme }) => theme.lightmode.colors.primary};
-  border: 2px solid ${({ theme }) => theme.lightmode.colors.secondary};
+  width: 14rem;
+  font-size: ${({ fs }) => fs || "1.4"}rem;
+  font-weight: 600;
+  color: white;
+  border: transparent;
   border-radius: 1rem;
-  background-color: ${({ theme }) => theme.lightmode.colors.background};
+  background-color: ${({ theme }) => theme.lightmode.colors.secondary};
   text-align: center;
   text-decoration: none;
+  transition: 0.15s;
   cursor: pointer;
 
-  margin-top: ${({ mgt }) => mgt || '0'};
-  margin-right: ${({ mgr }) => mgr || '0'};
-  margin-bottom: ${({ mgb }) => mgb || '0'};
-  margin-left: ${({ mgl }) => mgl || '0'};
+  margin-top: ${({ mgt }) => mgt || "0"};
+  margin-right: ${({ mgr }) => mgr || "0"};
+  margin-bottom: ${({ mgb }) => mgb || "0"};
+  margin-left: ${({ mgl }) => mgl || "0"};
 
+  &:hover {
+    box-shadow: 0 0 8px rgba(33, 33, 33, 0.2);
+  }
+  ${({ primary }) =>
+    primary &&
+    css`
+      height: 4.5rem;
+      width: 20rem;
+      font-size: 1.7rem;
+    `};
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: ${({ theme }) => theme.colors.lightmode.secondary};
+      background-color: ${({ theme }) => theme.info};
     `};
   ${({ big }) =>
     big &&
@@ -35,8 +47,21 @@ const Button = styled.button`
   ${({ info }) =>
     info &&
     css`
+      background-color: transparent;
       border: none;
       color: ${({ theme }) => theme.info};
+      &:hover {
+        box-shadow: none;
+
+        @media screen and ${device.laptop} {
+          transform: scale(1.2);
+        }
+      }
+    `};
+  ${({ warning }) =>
+    warning &&
+    css`
+      background-color: ${({ theme }) => theme.warning};
     `};
 `;
 
