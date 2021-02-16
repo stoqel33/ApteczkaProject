c2084f573bd65c84edbaa8aa9ffc23154fbb9e8b
stoqel33 <stochel202@interia.pl>
2021-02-16 14:20:59 +0100

    add feature automatically swtich to main page if token is being expired

--- client/src/data/Actions/errorActions.js
+++ client/src/data/Actions/errorActions.js
@@ -1,4 +1,6 @@
 import { GET_ERRORS, CLEAR_ERRORS } from "data/Actions/types";
+import { SET_CURRENT_USER } from "data/Actions/types";
+import setAuthToken from "data/Utils/setAuthToken";
 
 export const clearErrors = () => {
   return {
@@ -7,6 +9,14 @@ export const clearErrors = () => {
 };
 
 export const getErrors = (data) => {
+  if (data === "Unauthorized") {
+    localStorage.removeItem("jwtToken");
+    setAuthToken(false);
+    return {
+      type: SET_CURRENT_USER,
+      payload: {},
+    };
+  }
   return {
     type: GET_ERRORS,
     payload: data,
